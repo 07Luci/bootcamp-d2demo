0b6540222e564658e43f462ccc19c386e90fec57
07Luci <divyanshuupadhyay536@gmail.com>
2025-02-13 12:05:58 +0530

    feature add task

--- Addtask/script.js
+++ Addtask/script.js
@@ -3,12 +3,23 @@ let taskInput = document.getElementById("taskInput");
 let addTaskButton = document.getElementById("addTask");
 let taskList = document.getElementById("taskList");
 
+let taskText = taskInput.value.trim();
+
 // Function to add a task
 function addTask() {
+
+    //console.log("Button clicked")
+    let taskText = taskInput.value.trim();
+
+    let li = document.createElement("li");
+    // taskText.push
+    li.textContent = taskText;
+    taskList.appendChild(li);
 
     // take task text
 
     // Append task
+    taskInput.value="";
 
     // Clear input
 }
